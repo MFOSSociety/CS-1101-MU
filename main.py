c25cd3e664a4e3a8f6b17bb1fc0bb2eb3ffb77fb
Piyush Raj <piyushraj.4680@gmail.com>
2018-07-30 18:07:45 +0530

    Added main.py having octal/binary to decimal functions

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+def binary2decimal(binary):
+  i=str(binary)
+  n=len(i)- 1
+  f=0
+  for e in i:
+   f=f+int(e)*(2**n)
+   n=n-1
+  print(f)
+  
+
+def octal2decimal(oct):
+  i=str(oct)
+  n=len(i)- 1
+  f=0
+  for e in i:
+   f=f+int(e)*(8**n)
+   n=n-1
+  print(f)
